77ebcf1ac8e27b63336e7ed96ee79ee50079573a
Evgenii Sidorov <sidzheone@gmail.com>
2023-04-17 12:15:25 +0700

    add teamModel

---
+++ Crocodile/TeamModel/TeamModel.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Crocodile
+//
+//  Created by sidzhe on 17.04.2023.
+//
+
+import UIKit
+
+struct TeamModel {
+    var name: String
+    var image: UIImage
+    var score: Int
+}
+
