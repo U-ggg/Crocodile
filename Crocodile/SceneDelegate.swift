52054ec383cb5176e669043c39cc43f9d2124c78
Sergey Medvedev <devgod@mail.ru>
2023-04-20 14:49:33 +0300

    refactoring

--- Crocodile/SceneDelegate.swift
+++ Crocodile/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: CorrectViewController())
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
